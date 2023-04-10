bd44d7b5117ed7e3e536ecf8dde94d89ff36a2b4
SUHEL Makkad <MAKADSUHEL11@GMAIL.COM>
2023-04-10 10:19:12 +0530

    moved imageg data to seprate file

---
+++ src/utils/imgData.ts
@@ -0,0 +1,35 @@
+export const images = {
+  users: {
+    andry: {
+      src: "/img/user/andry.png",
+      height: 85,
+      width: 82,
+    },
+  },
+  products: {
+    tab: {
+      src: "/img/product/tab.jpg",
+      height: 675,
+      width: 675,
+    },
+    watch: {
+      src: "/img/product/watch.jpg",
+      height: 940,
+      width: 827,
+    },
+  },
+  landscapeModels: {
+    green: {
+      src: "/img/models/landscape/1.jpg",
+      height: 518,
+      width: 910,
+    },
+  },
+  portraitModels: {
+    gray: {
+      src: "/img/models/portrait/1.jpg",
+      height: 518,
+      width: 440,
+    },
+  },
+};
