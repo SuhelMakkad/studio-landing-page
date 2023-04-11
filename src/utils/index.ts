9a4a2adfa39e4f60b7b08c17d68698cc5d21861f
SUHEL Makkad <MAKADSUHEL11@GMAIL.COM>
2023-04-11 11:14:15 +0530

    showing hero gradent to fuill section

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,5 @@
 export const PROJECT_BASE = "/studio-landing-page";
 
 export const PORTFOLIO_URL = "https://suhel.vercel.app";
+
+export const DESIGN_URL = "https://www.uistore.design/items/landing-page-free-ui-kit-for-figma"
